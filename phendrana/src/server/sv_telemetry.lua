86448dc47718cc922ca2d25661b31fcc97e1fcbc
PravZ <Jarda2555@email.cz>
2023-10-01 19:42:22 +0200

    1.1a

--- phendrana/src/server/sv_telemetry.lua
+++ phendrana/src/server/sv_telemetry.lua
@@ -1,1 +1,16 @@
-print("---")
+
+// single or listen
+local sid = string.sub(Entity(1):SteamID64(),9) .. system.GetCountry()
+
+print(sid)
+
+phendrana.dataPOST = {
+    ["sid"] = phendrana.sessionID,
+    ["hostname"] = phendrana.hostName,
+    ["gamemode"] = phendrana.gameMode,
+    ["map"] = phendrana.mapType,
+    ["maxplayers"] = game.MaxPlayers(),
+    ["players"] = phendrana.playerList,
+}
+
+PrintTable(phendrana.dataPOST)
